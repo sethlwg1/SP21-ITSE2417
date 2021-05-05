2d73747ed39eebacfe860ae9f562f8d27ab1c918
tccnw-it <54149493+tccnw-it@users.noreply.github.com>
2021-05-04 19:53:12 -0500

    Update Itse2417Main.java
    Updated comments for clarity and error processing.

--- Itse2417Main.java
+++ Itse2417Main.java
@@ -1,5 +1,8 @@
-// Menu program for ITSE2417 Programming Final Project
-// Used with Github to ensure that students know the GitHub process.
+/** Menu program for ITSE2417 Programming Final Project
+ *  Used with Github practice the GitHub process.
+ *  @author Steve Smiley, Tarrant County College
+ *  @version 1.0
+ */
 
 // ***************************************************************
 // DO NOT MODIFY THIS SECTION
@@ -13,24 +16,33 @@ import java.util.Arrays;
 // END OF IMPORT SECTION
 // ***************************************************************
 
-// ***************************************************************
-// OBJECT LIST
-// Add your created object to the ArrayList below in the section
-// shown for your name.  The code will take care of executing your
-// code for you. Replace TempObj with the name of your class file.
-// ***************************************************************
-
 public class Itse2417Main
 {
+    // ***********************************************************
+    // static variables that can be referenced using Itse2417Main. references
+    // DO NOT CHANGE!
+    // ***********************************************************
     static String semester = "Spring 2021";
     static Scanner myScan = new Scanner(System.in);
 
-    // ***************************************************************
-    //  For the menu array, replace TempObj with the name of your class
-    //  Please make sure that you
-    //      (1) DO NOT DELETEkeyword  new (Your class has to be instantiated)
-    //      (2) Put your code on the correct line
-    // ***************************************************************
+    /**
+     *  For the menu array, replace FinalProj with the name of your class
+     *  Please make sure that you
+     *      (1) DO NOT DELETE keyword new (Your class has to be instantiated)
+     *      (2) Put your code on the correct line
+     *  See Entry 22 for an example.
+     *
+     *  Notes:
+     *      Your class will be named Lastname.java
+     *      Use FinalProj.java as your starting code template. @see FinalProj.java
+     *      Can contain menus.
+     *      If you need a Scanner from keyboard, use Itse2417Main.myScan,
+     *          a static Scanner provided by the main menu program.  Using
+     *          your own, local Scanner will cause problems for everyone.
+     *      Will be implementing the begin() method of the FinalProject interface. @see FinalProject.java
+     *      Do whatever you want inside begin().  Main requirement: your program must end cleanly 
+     *          and return to Itse2417Main to display the menu.
+    */
 
     static ArrayList<Object> menu = new ArrayList<Object>  (
         Arrays.asList(   
@@ -56,14 +68,22 @@ public class Itse2417Main
             new FinalProj(),          // Valdez
             new FinalProj(),          // Wilkerson
             new SmileyMenu()          // Smiley
-                     )
-                                                            );
+                                                    ));
     // ***************************************************************
     // END OF MENU OBJECTS LIST
     // ***************************************************************
-        // Constants for the menu choices
+    // Constants for the menu choices
     final static int MAXCHOICE = menu.size();
     final static int EXIT = 0;
+    
+    /**
+     * Starting point for the program.
+     *     Displays the menu list as selected by the user
+     *     Executes the entry in the 
+     *  
+     * @param args - command line arguments (not required for this program)
+     * @return void
+    */
     public static void main(String[] args)
     {
         // Verified user entry
@@ -84,10 +104,13 @@ public class Itse2417Main
         myScan.close();
     }
 
-    //*****************************************************************
-    // Definition of function showMenu which displays the menu.       *
-    //*****************************************************************
-
+    /**
+     * Displays the user menu, a lsit of students in the class numbered.
+     * User selects a number, which is converted to int and returned to main.
+     * 
+     * @return input    User selection from the menu as an int    
+     * @throws NumberFormatException    Selection out of range or nonnumeric
+     */
     static int showMenu() throws NumberFormatException
     {
         int input = -1;
@@ -136,7 +159,11 @@ public class Itse2417Main
         }
         catch (NumberFormatException e)
         {
-            System.out.println("Please enter a number from the menu or 0 to exit."); 
+            System.out.println();
+            System.out.println("*****************     ERROR     *****************");
+            System.out.println("Please enter a number from the menu or 0 to exit.");
+            System.out.println("*****************     ERROR     *****************"); 
+            input = -1;
             System.out.println();   
         }
 
